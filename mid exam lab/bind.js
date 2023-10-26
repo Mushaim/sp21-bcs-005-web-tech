37c519f92d57751cb25a9521b38b9b67704cbd83
Mushaim Khan <mushaimk01@gmail.com>
2023-10-26 15:54:28 +0500

    exam

---
+++ mid exam lab/bind.js
@@ -0,0 +1,15 @@
+function doBindings() {
+  
+    let logo = document.getElementById("logo");
+    logo.onclick = removeMainMenu; 
+  }
+  
+  window.onload = doBindings;
+  
+  function removeMainMenu() {
+    let mainMenu = document.getElementById("main-menu"); 
+    if (mainMenu) {
+      mainMenu.style.display = "none";
+    }
+  }
+  
